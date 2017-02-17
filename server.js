bd7141ffa39cbb90107fc8783d5c698d4c2578ba
Daniel Segovia <daniel@dabici.mx>
2017-02-16 23:08:50 -0600

    Testing in heroku

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var express = require("express")
 var app = express()
-var i = 0;
 
-app.get('/', function (req, res) {
-	i++
-	res.end('hello world i = ' + i)
+var output = {
+	"unix": 0,
+	"natural": ""
+}
+
+console.log(output)
+
+function outputNaturalDate(date) {
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	return months[date.getMonth()] + " " + (date.getDate() + 1) + ", " + date.getFullYear()
+}
+
+app.use('/:time', function (req, res) {
+	time = req.params.time
+
+	if ((/^\d+/g).test(time)) {
+		console.log("1st")
+		output.unix = parseInt(time)
+		output.natural = outputNaturalDate(new Date(time*1000))
+	}else if ((/^(January|February|March|April|May|June|July|August|September|October|November|December)\s\d+,\s\d+/g).test(time)) {
+		console.log("2nd")
+		output.unix = Date.parse(time)/1000
+		output.natural = time
+	}else{
+		console.log("3rd")
+		output.unix = null
+		output.natural = null
+	}
+		
+		
+	// }
+		res.end(JSON.stringify(output))
 })
 
 app.listen(3000, function(){
